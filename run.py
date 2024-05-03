d6b435791e47ee1ea945623762d02e651a1299cc
Bhagyashri Patil <bhagyashriyogeshpatil@gmail.com>
2024-05-03 10:12:29 +0000

    Add function to get stock values and display sandwich production requirements for the upcoming market

--- run.py
+++ run.py
@@ -118,6 +118,16 @@ def calculate_stock_data(data):
     return new_stock_data
 
 
+def get_stock_values(data):
+    """
+    Retrieve headings from the worksheet and create a dictionary.
+    """
+    headings = SHEET.worksheet("stock").row_values(1)
+    print("Make the following numbers of sandwiches for next market:\n")
+    stock_values = {headings[i]: data[i] for i in range(len(headings))}
+    return stock_values
+
+
 def main():
     """
     Run all program functions
@@ -130,7 +140,12 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    stock_values = get_stock_values(stock_data)
+    print(stock_values)
 
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
+
+
+
